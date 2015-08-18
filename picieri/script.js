997c6668ae46d6f7b16834cc658585765986db44
StefanPrifti <s.prifti.al@gmail.com>
2015-08-18 11:56:10 +0200

    Angular controller edit

--- picieri/script.js
+++ picieri/script.js
@@ -4,5 +4,5 @@ var app = angular.module('app', []);
 
 app.controller('mainData', function($scope, $http) {
     $http.get("http://porosit-pica.herokuapp.com/api/getorderedpizzas")
-    .success(function (response) {$scope.pizzaList = response;});;
+    .success(function (response) {$scope.pizzaList = response;});
 });
