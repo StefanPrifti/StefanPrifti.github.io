26c3252969f8cb1b699a0b9d7ca97142df075b77
StefanPrifti <s.prifti.al@gmail.com>
2015-08-18 15:20:40 +0200

    Angular controller edit

--- picieri/script.js
+++ picieri/script.js
@@ -7,8 +7,18 @@ app.controller('mainData', function($scope, $http) {
     .success(function (response) {$scope.pizzaList = response;});
 
     $scope.listID = 0;
+    $scope.selectedPizza = null;
+    $scope.isSelected = false;
 
     $scope.setListID = function(id) {
 		$scope.listID = id;
 	}
+
+	$scope.selectPizza = function(pizzaID) {
+		$scope.selectedPizza = pizzaID;
+		$scope.isSelected = true;
+	}
+
+	$scope.selectedPizza = $filter('filter')($scope.pizzaList, function (p) {return p._id == $scope.selectedPizza;})[0];
+	console.log($scope.selectedPizza);
 });
