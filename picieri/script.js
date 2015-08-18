393c5cceaba8d4ea5f20edd8b013ad46f3b600cb
StefanPrifti <s.prifti.al@gmail.com>
2015-08-18 19:03:55 +0200

    Angular controller edit

--- picieri/script.js
+++ picieri/script.js
@@ -22,7 +22,7 @@ app.controller('mainData', function($scope, $http, $filter) {
 		$http.get("http://porosit-pica.herokuapp.com/api/getPizzaDetails/" + pizzaID)
 	    .success(function (response) {
 	    	$scope.selectedPizza = response[0];
-	   		$scope.selectedPizzaDetails  = $filter('filter')($scope.pizzaList, function (p) { console.log(p); })[0];
+	   		$scope.selectedPizzaDetails  = $filter('filter')($scope.pizzaList, function (p) { console.log(p.pizza_user_template); })[0];
 	    });
 
 	}
