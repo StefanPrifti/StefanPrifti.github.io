3d53c5f7c84425651cc24ac754b2633d495caf61
StefanPrifti <s.prifti.al@gmail.com>
2015-08-18 15:33:38 +0200

    Angular controller edit

--- picieri/script.js
+++ picieri/script.js
@@ -4,7 +4,10 @@ var app = angular.module('app', []);
 
 app.controller('mainData', function($scope, $http, $filter) {
     $http.get("http://porosit-pica.herokuapp.com/api/getorderedpizzas")
-    .success(function (response) {$scope.pizzaList = response;});
+    .success(function (response) {
+    	$scope.pizzaList = response;
+		$scope.selectedPizza = $filter('filter')($scope.pizzaList, function (p) {return p._id == $scope.selectedPizza;})[0];
+    });
 
     $scope.listID = 0;
     $scope.selectedPizza = null;
@@ -17,8 +20,8 @@ app.controller('mainData', function($scope, $http, $filter) {
 	$scope.selectPizza = function(pizzaID) {
 		$scope.selectedPizza = pizzaID;
 		$scope.isSelected = true;
+		$scope.selectedPizza = $filter('filter')($scope.pizzaList, function (p) {return p._id == $scope.selectedPizza;})[0];
 	}
 
-	$scope.selectedPizza = $filter('filter')($scope.pizzaList, function (p) {return p._id == $scope.selectedPizza;})[0];
 	console.log($scope.selectedPizza);
 });
