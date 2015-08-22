86a450fd7f9bc8c80c647e8e831354a287ab6d35
StefanPrifti <s.prifti.al@gmail.com>
2015-08-22 22:26:53 +0200

    Working on pizzaiolo

--- picieri/script.js
+++ picieri/script.js
@@ -21,7 +21,7 @@ app.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
         });
 
         // use the HTML5 History API
-        $locationProvider.html5Mode(true);
+        //$locationProvider.html5Mode(true);
         
 });
 
